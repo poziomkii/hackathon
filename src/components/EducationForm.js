a0705679a7475f8f69eba782ee9b3e27fe33dd9e
barosiak <basiarosiak.7@gmail.com>
2022-11-26 12:35:20 +0100

    Fixed frontend of forms

--- src/components/EducationForm.js
+++ src/components/EducationForm.js
@@ -1,77 +1,102 @@
+import "./style/Form.css";
 import React from "react";
-import { Button } from '@chakra-ui/react'
-import { Select } from '@chakra-ui/react'
-import { Stack } from '@chakra-ui/react'
-import { Radio, RadioGroup } from '@chakra-ui/react'
+import { Button } from "@chakra-ui/react";
+import { Select } from "@chakra-ui/react";
+import { Stack } from "@chakra-ui/react";
+import { Box, Heading, Radio, RadioGroup } from "@chakra-ui/react";
 import {
-    NumberInput,
-    NumberInputField,
-    NumberInputStepper,
-    NumberIncrementStepper,
-    NumberDecrementStepper,
-  } from '@chakra-ui/react'
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  NumberIncrementStepper,
+  NumberDecrementStepper,
+} from "@chakra-ui/react";
 
 class EducationForm extends React.Component {
-    render() {
-        return(
-            <div>
-                <div>
-                <Select name='university' placeholder='University' onChange={(event) => this.props.handleChange(event)}>
-                    <option value='University of Warsaw'>University of Warsaw</option>
-                    <option value='Warsaw University of Technology'>Warsaw University of Technology</option>
-                </Select>
-                </div>
-                <div>
-                <Select name='field' placeholder='Field of studies' onChange={(event) => this.props.handleChange(event)}>
-                    <option value='Computer Science'>Computer Science</option>
-                    <option value='Mathematics'>Mathematics</option>
-                    <option value='Engineering'>Engineering</option>
-                    <option value='Engineering'>Other</option>
-                </Select>
-                </div>
-                <div>
-                <Select name='degree' placeholder='Degree' onChange={(event) => this.props.handleChange(event)}>
-                    <option value='Bachelors'>Bachelors</option>
-                    <option value='Masters'>Masters</option>
-                </Select>
-                </div>
-                <div>Graduation date</div>
-                <Stack shouldWrapChildren direction='row'>
-                    <NumberInput min={1} max={12} onChange={(event) => this.props.handleNumber('grad_month', event)} >
-                        <NumberInputField />
-                        <NumberInputStepper>
-                        <NumberIncrementStepper />
-                        <NumberDecrementStepper />
-                        </NumberInputStepper>
-                    </NumberInput>
-                    <NumberInput min={2022} onChange={(event) => this.props.handleNumber('grad_year', event)}>
-                        <NumberInputField />
-                        <NumberInputStepper>
-                        <NumberIncrementStepper />
-                        <NumberDecrementStepper />
-                        </NumberInputStepper>
-                    </NumberInput>
-                </Stack>
-                <div>
-                <div>Do you plan further education?</div>
-                <RadioGroup onChange={(event) => this.props.handleNumber('further_education', event)}>
-                    <Stack spacing={5} direction='row'>
-                        <Radio colorScheme='green' value='Yes'>
-                        Yes
-                        </Radio>
-                        <Radio colorScheme='red' value='No'>
-                        No
-                        </Radio>
-                    </Stack>
-                </RadioGroup>
-            </div>
-            <div><Button
-                onClick={() =>this.props.validateEducationForm()}>
-                Next
-            </Button></div>   
-            </div>
-        );
-    }
+  render() {
+    return (
+      <Box w={[500, 1000]} className="form">
+        <Select
+          name="university"
+          placeholder="University"
+          onChange={(event) => this.props.handleChange(event)}
+        >
+          <option value="University of Warsaw">University of Warsaw</option>
+          <option value="Warsaw University of Technology">
+            Warsaw University of Technology
+          </option>
+        </Select>
+        <Select
+          name="field"
+          placeholder="Field of studies"
+          onChange={(event) => this.props.handleChange(event)}
+        >
+          <option value="Computer Science">Computer Science</option>
+          <option value="Mathematics">Mathematics</option>
+          <option value="Engineering">Engineering</option>
+          <option value="Engineering">Other</option>
+        </Select>
+        <Select
+          name="degree"
+          placeholder="Degree"
+          onChange={(event) => this.props.handleChange(event)}
+        >
+          <option value="Bachelors">Bachelors</option>
+          <option value="Masters">Masters</option>
+          <option value="Doctorate">Doctorate</option>
+        </Select>
+        <Heading size="sm">Graduation date</Heading>
+        <Stack shouldWrapChildren direction="row">
+          <NumberInput
+            min={1}
+            max={12}
+            defaultValue={1}
+            onChange={(event) => this.props.handleNumber("grad_month", event)}
+          >
+            <NumberInputField />
+            <NumberInputStepper>
+              <NumberIncrementStepper />
+              <NumberDecrementStepper />
+            </NumberInputStepper>
+          </NumberInput>
+          <NumberInput
+            min={2020}
+            defaultValue={2022}
+            onChange={(event) => this.props.handleNumber("grad_year", event)}
+          >
+            <NumberInputField />
+            <NumberInputStepper>
+              <NumberIncrementStepper />
+              <NumberDecrementStepper />
+            </NumberInputStepper>
+          </NumberInput>
+        </Stack>
+        <Heading size="sm">Do you plan further education?</Heading>
+        <RadioGroup
+          onChange={(event) =>
+            this.props.handleNumber("further_education", event)
+          }
+        >
+          <Stack spacing={5} direction="row">
+            <Radio colorScheme="green" value="Yes">
+              Yes
+            </Radio>
+            <Radio colorScheme="red" value="No">
+              No
+            </Radio>
+          </Stack>
+        </RadioGroup>
+        <Button
+          width={[100, 200, 350]}
+          size="lg"
+          colorScheme="blue"
+          onClick={() => this.props.validateEducationForm()}
+        >
+          Next
+        </Button>
+      </Box>
+    );
+  }
 }
 
 export default EducationForm;
--- src/components/MetricsForm.js
+++ src/components/MetricsForm.js
@@ -1,37 +1,54 @@
+import "./style/Form.css";
 import React from "react";
-import { Button } from '@chakra-ui/react'
-import { Select } from '@chakra-ui/react'
+import { Button, Box, Heading, Select } from "@chakra-ui/react";
 
 class MetricsForm extends React.Component {
-    render() {
-        return(
-            <div>
-                Note that answering these questions is voluntary, but helps us mesure diversity in our company.
-                <div>
-                <Select name='gender' placeholder='Gender' onChange={(event) => this.props.handleChange(event)}>
-                    <option value='Male'>Male</option>
-                    <option value='Female'>Female</option>
-                    <option value='Non-binary'>Non-binary</option>
-                    <option value='Prefer not to say'>Prefer not to say</option>
-                </Select>
-                </div>
-                <div>
-                <Select name='ethnicity' placeholder='Ethnicity' onChange={(event) => this.props.handleChange(event)}>
-                    <option value='White'>White</option>
-                    <option value='Black or African American'>Black or African American</option>
-                    <option value='American Indian or Alaska Native'>American Indian or Alaska Native</option>
-                    <option value='Asian'>Asian</option>
-                    <option value='Native Hawaiian or Other Pacific Islander'>Native Hawaiian or Other Pacific Islander</option>
-                    <option value='Prefer not to say'>Prefer not to say</option>
-                </Select>
-                </div>
-                <div><Button
-                    onClick={() =>this.props.validateMetricsForm()}>
-                    Next
-                </Button></div>   
-            </div>
-        );
-    }
+  render() {
+    return (
+      <Box w={[500, 1000]} className="form">
+        <Heading size="sm">
+          Note that answering these questions is voluntary, but helps us mesure
+          diversity in our company.
+        </Heading>
+        <Select
+          name="gender"
+          placeholder="Gender"
+          onChange={(event) => this.props.handleChange(event)}
+        >
+          <option value="Male">Male</option>
+          <option value="Female">Female</option>
+          <option value="Non-binary">Non-binary</option>
+          <option value="Prefer not to say">Prefer not to say</option>
+        </Select>
+        <Select
+          name="ethnicity"
+          placeholder="Ethnicity"
+          onChange={(event) => this.props.handleChange(event)}
+        >
+          <option value="White">White</option>
+          <option value="Black or African American">
+            Black or African American
+          </option>
+          <option value="American Indian or Alaska Native">
+            American Indian or Alaska Native
+          </option>
+          <option value="Asian">Asian</option>
+          <option value="Native Hawaiian or Other Pacific Islander">
+            Native Hawaiian or Other Pacific Islander
+          </option>
+          <option value="Prefer not to say">Prefer not to say</option>
+        </Select>
+        <Button
+          width={[100, 200, 350]}
+          size="lg"
+          colorScheme="blue"
+          onClick={() => this.props.validateMetricsForm()}
+        >
+          Next
+        </Button>
+      </Box>
+    );
+  }
 }
 
 export default MetricsForm;
