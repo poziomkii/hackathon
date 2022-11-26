4a9866cbf75c8cf62660cc8f6a330a35afea89b5
Paweł Putra <pawel.putra@scylladb.com>
2022-11-26 05:00:17 +0100

    game: added stage changing logic
    Now game keeps track of which stage the user is currently on,
    and renders the appropriate stage by dynamically rendering a react component
    specified inside stages array under current stage index.

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,18 +1,33 @@
 import './style/Game.css';
 import React from "react";
 import { Heading } from "@chakra-ui/react";
+import Start from "./Start.js"
 
 class Game extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      stage: 0,
+    };
+  }
+
+  nextStage() {
+    this.setState({
+      stage: this.state.stage + 1,
+    })
   }
 
   render() {
+    const stages = [
+      () => <Start stage={this.state.stage} onClick={() => this.nextStage()}></Start>,
+      () => <Start stage={this.state.stage} onClick={() => this.nextStage()}></Start>,
+      () => <Start stage={this.state.stage} onClick={() => this.nextStage()}></Start>,
+      () => <Start stage={this.state.stage} onClick={() => this.nextStage()}></Start>,
+    ]
     return (
       <div className="game">
         <Heading>Let's get to know each other :)</Heading>
-        <div className="game-body">{/* body */}</div>
+        <div className="game-body">{stages[this.state.stage]()}</div>
       </div>
     );
   }
